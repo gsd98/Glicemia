67e9de0e974d931ca0811bc1e2c5333eab790512
gsilv <gsilv@GUSTA>
2023-05-31 23:44:26 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+using static System.Convert;
+
+
+WriteLine("Nome do Paciente:");
+ReadLine();
+WriteLine("Idade do Paciente:");
+ReadLine();
+WriteLine("Cidade e Estado aonde reside:");
+ReadLine();
+int n1;
+WriteLine("Digite Sua Glicemia:");
+n1 = ToInt32(ReadLine());
+if (n1 >= 200)
+{
+    WriteLine("Sua Glicemia está Alta! Tome Insulina!");
+}
+else
+{
+    if (n1 < 80)
+    {
+        WriteLine("Sua Glicemia está Baixa! Coma açúcar para estabilizar!");
+    }
+    else
+    WriteLine("Sua Glicemia está Normal! Continue se cuidando!");
+}
+ReadKey();
+
+
+
